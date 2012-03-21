f9a35575dc884b7f030effd8e195d51a65a96264
Aaron Tian <Aaron2Ti@gmail.com>
2012-03-21 22:41:36 +0800

    all methods should be private

--- lib/memor.rb
+++ lib/memor.rb
@@ -2,6 +2,8 @@ require 'memor/version'
 
 module Memor
 
+private
+
   # Example
   #
   # class Foo
@@ -39,7 +41,6 @@ module Memor
     end
   end
 
-
   def _memor_args(memor_method, context_binding)
     arg_names = _memor_arg_names(memor_method)
 
@@ -47,7 +48,6 @@ module Memor
       context_binding.eval arg_name.to_s
     end
   end
-  private :_memor_args
 
   def _memor_arg_names(memor_method)
     # parameters is like [[:req, :a], [:req, :b], [:rest, :c], [:block, :d]]
@@ -57,7 +57,6 @@ module Memor
       end
     end.compact
   end
-  private :_memor_arg_names
 
   def _memor_with_no_arg(memor_name)
     unless instance_variable_defined? memor_name
@@ -66,7 +65,6 @@ module Memor
 
     instance_variable_get memor_name
   end
-  private :_memor_with_no_arg
 
   def _memor_with_args(memor_name, args)
     unless instance_variable_defined? memor_name
@@ -82,6 +80,5 @@ module Memor
 
     memor[key]
   end
-  private :_memor_with_args
 
 end
